e496cba7416840a0e2d8588e79b40cef99e3b3f1
Amanda Flynn <amandaflynn98@gmail.com>
2025-01-06 11:12:47 +0000

    added the 'heroku' part to allowed hosts to see what/ if it works

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-p39ic0^z4mj6*mp9zkd4j&uec(ou!ogbhsf&)vg)=$j#6t_=r=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
